f270a6cf8702dbc1c8c21fa920aa7d698db91e37
Juan Navarlaz Urtubia <jnavarlaz002@ikasle.ehu.eus>
2023-11-07 11:38:18 +0100

    patrón Factory

--- src/factory/Main.java
+++ src/factory/Main.java
@@ -1,13 +1,16 @@
 package factory;
 
+import domain.Covid19Factory;
 import domain.Covid19Pacient;
 import domain.Medicament;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Covid19Pacient p1=new Covid19Pacient("aitor", 35);
+		Covid19Pacient p1=new Covid19Pacient("aitor", 35,new Covid19Factory());
 		new PacientSymptomGUI(p1);
+		Medicament	m=new Medicament("Ibuprofeno",new Covid19Factory());
+		MedicalGUI	mgui=new MedicalGUI(m);
 
 	}
 
